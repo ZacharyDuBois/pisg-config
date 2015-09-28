00a91afc6f3268bdb6b5127dcbe8a5a04961fbb1
Zachary DuBois <zacharydubois@icloud.com>
2015-09-27 22:35:00 -0400

    Add gen script.

---
+++ script/gen.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Pull new updates from GitHub
+cd /srv/pisg/pisg-config/
+git pull
+
+# Render all configs.
+for config in /srv/pisg/pisg-config/*.cfg
+do
+  /srv/pisg/pisg --configfile=$config
+done
